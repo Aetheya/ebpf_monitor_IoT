476a54082d3e5802b7f6d38c4dbb885812d9c06d
Aetheya <aetheya@gmail.com>
2019-07-26 13:17:58 +0200

    test protocol separation

--- ebpf_map_stat.c
+++ ebpf_map_stat.c
@@ -57,7 +57,7 @@ int detect_rcv_pkts(struct pt_regs *ctx,struct sk_buff *skb,struct sock *sk){
     proto_map.increment(protocol);
     u8 key2 = 7;
 
-    if (protocol == IPPROTO_TCP) { proto_map.increment(key2);}
+    //if (protocol == IPPROTO_TCP) { proto_map.increment(key2);}
 
     u64 key= 0;
     stats_map.increment(key);
@@ -65,7 +65,31 @@ int detect_rcv_pkts(struct pt_regs *ctx,struct sk_buff *skb,struct sock *sk){
 }
 
 int detect_snt_pkts(struct pt_regs *ctx, struct sk_buff *skb){
+u8 protocol = 0;
+    //struct sock *newsk = (struct sock *)PT_REGS_RC(ctx);µ
+        struct sock *newsk = sk;
 
+
+    int gso_max_segs_offset = offsetof(struct sock, sk_gso_max_segs);
+    int sk_lingertime_offset = offsetof(struct sock, sk_lingertime);
+    if (sk_lingertime_offset - gso_max_segs_offset == 4)
+        // 4.10+ with little endian
+#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+        protocol = *(u8 *)((u64)&newsk->sk_gso_max_segs - 3);
+    else
+        /// pre-4.10 with little endian -> renvoie 0 tt ltemps
+        protocol = *(u8 *)((u64)&newsk->sk_wmem_queued - 3);
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+        // 4.10+ with big endian
+        protocol = *(u8 *)((u64)&newsk->sk_gso_max_segs - 1);
+    else
+        // pre-4.10 with big endian -> renvoie 0 tt ltemps
+        protocol = *(u8 *)((u64)&newsk->sk_wmem_queued - 1);
+#else
+# error "Fix your compiler's __BYTE_ORDER__?!"
+#endif
+    proto_map.increment(protocol);
+    u8 key2 = 7;
 
     u64 key= 1;
     stats_map.increment(key);
