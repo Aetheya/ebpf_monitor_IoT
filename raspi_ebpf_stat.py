6699c87a0b9667a2ace1ac1871e5c6cdfef18015
Aetheya <aetheya@gmail.com>
2019-07-26 17:35:55 +0200

    test loss pkt

--- raspi_ebpf_stat.py
+++ raspi_ebpf_stat.py
@@ -99,7 +99,7 @@ def start_ebpf():
     b.attach_kprobe(event="ip_output", fn_name="detect_dport")
     b.attach_kprobe(event="ip_output", fn_name="detect_family")
     b.attach_kprobe(event="ip_rcv", fn_name="detect_family")
-    b.attach_kprobe(event="tcp_enter_loss", fn_name="detect_lost_pkts")
+    b.attach_kprobe(event="tcp_retransmit_skb", fn_name="detect_lost_pkts")
 
 
 def stop_ebpf():
