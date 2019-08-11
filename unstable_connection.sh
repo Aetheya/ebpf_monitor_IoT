eab147dd4d839bdc37900e954b40c32cb6720935
Sophie Madessis <aetheya@gmail.com>
2019-08-11 17:27:32 +0200

    testing_process add

--- unstable_connection.sh
+++ unstable_connection.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 date
-
+bash drop_pkts.sh;
 for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
 sleep 3
 date
-bash drop_pkts.sh;
+
 for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
 date
 bash reset_iptables.sh;
