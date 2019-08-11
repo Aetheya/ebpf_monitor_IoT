7071e944024570bbc13baecec3ea199e25393a31
Sophie Madessis <aetheya@gmail.com>
2019-08-11 17:24:22 +0200

    testing_process add

--- drop_pkts.sh
+++ drop_pkts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-iptables -A OUTPUT -m statistic --mode random --probability 0.15 -j DROP
-iptables -A INPUT -m statistic --mode random --probability 0.15 -j DROP
+iptables -A OUTPUT -m statistic --mode random --probability 0.1 -j DROP
+iptables -A INPUT -m statistic --mode random --probability 0.1 -j DROP
 iptables -L
--- unstable_connection.sh
+++ unstable_connection.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 date
 
-#for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
-#sleep 3
-#date
-#bash drop_pkts.sh;
-#for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
-#date
-#bash reset_iptables.sh;
-#for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
-
+for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
+sleep 3
+date
 bash drop_pkts.sh;
-sudo tcpdump;
+for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
+date
+bash reset_iptables.sh;
+for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
