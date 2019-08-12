ef2baa5e004ff7a75992acd638e3fde583ea5d63
Sophie Madessis <aetheya@gmail.com>
2019-08-12 14:25:29 +0200

    test server tcp

--- tcp_client.py
+++ tcp_client.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 8888)
+server_address = ('192.168.1.10', 8888)
 print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
--- tcp_server.py
+++ tcp_server.py
@@ -4,7 +4,7 @@ import sys
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
-server_address = ('localhost', 8888)
+server_address = ('192.168.1.10', 8888)
 print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 # Listen for incoming connections
