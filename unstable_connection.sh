cd92159581a1ff47dd20ece5f46af2dc98c3b7cf
Sophie Madessis <aetheya@gmail.com>
2019-08-12 14:44:17 +0200

    test server tcp

--- unstable_connection.sh
+++ unstable_connection.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
-date
 bash drop_pkts.sh;
-for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
-sleep 3
-date
-
-for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
-date
+sleep 1
 bash reset_iptables.sh;
-for i in `seq 10`; do curl --location --request GET "https://postman-echo.com/get?foo1=bar1&foo2=bar2"; done
+sleep1
+bash drop_pkts.sh;
+sleep 1
+bash reset_iptables.sh;
+sleep 1
+bash drop_pkts.sh;
+sleep 1
+bash reset_iptables.sh;
+sleep 1
+bash drop_pkts.sh;
+sleep 1
+bash reset_iptables.sh;
+sleep1
+bash drop_pkts.sh;
+sleep 1
+bash drop_pkts.sh;
+sleep 1
+bash reset_iptables.sh;
+sleep1
+bash drop_pkts.sh;
+sleep 1
+bash reset_iptables.sh;
