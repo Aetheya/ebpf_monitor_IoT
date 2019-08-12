c811569d78e9f9b15a2b1118c32be162c9d5b9f5
Sophie Madessis <aetheya@gmail.com>
2019-08-12 15:11:05 +0200

    test server tcp

--- tcp_client.py
+++ tcp_client.py
@@ -11,10 +11,11 @@ print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
 try:
+        print('Running')
         while True:
                 # Send data
                 message = 'This is the message.  It will be repeated.'
-                print('sending "%s"' % message)
+                #print('sending "%s"' % message)
                 sock.sendall(message)
 
                 # Look for the response
@@ -24,7 +25,7 @@ try:
                 while amount_received < amount_expected:
                         data = sock.recv(16)
                         amount_received += len(data)
-                        print('received "%s"' % data)
+                        #print('received "%s"' % data)
                 sleep(1)
 
 finally:
