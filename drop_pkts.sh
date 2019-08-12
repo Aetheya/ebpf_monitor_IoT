075f7211fab5fcf5092b51fd02658578a2e0676c
Sophie Madessis <aetheya@gmail.com>
2019-08-12 15:39:25 +0200

    test server tcp

--- drop_pkts.sh
+++ drop_pkts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-iptables -A OUTPUT -m statistic --mode random -p tcp --match multiport --dports 80,443,8888 --probability 0.13 -j DROP
-iptables -A INPUT -m statistic --mode random -p tcp --match multiport --dports 80,443,8888 --probability 0.13 -j DROP
+iptables -A OUTPUT -m statistic --mode random -p tcp --match multiport --dports 32876,33038 --probability 0.13 -j DROP
+iptables -A INPUT -m statistic --mode random -p tcp --match multiport --dports 32876,33038 --probability 0.13 -j DROP
 #iptables -A OUTPUT -m statistic --mode random --probability 0.13 -j DROP
 #iptables -A INPUT -m statistic --mode random --probability 0.13 -j DROP
 iptables -L
