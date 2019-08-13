80f0551148e46953f8e8d037c13f37da85c44063
Sophie Madessis <aetheya@gmail.com>
2019-08-13 13:59:05 +0200

    test server tcp

--- monitor_ebpf.py
+++ monitor_ebpf.py
@@ -216,9 +216,9 @@ def cmd_thresh(init_address, command):
     last_moment_sent = time.time()  # Last time data was sent
     while time.time() < future:
         if time.time() - last_moment_sent > command['interval']:
-            last_moment_sent = time.time()
             clean_maps()
             logger.info('Next period')
+            last_moment_sent = time.time()
         start_time_global = time.time()
         b.perf_buffer_poll(10000)  # Block until event happens or TO after 10sec
 
